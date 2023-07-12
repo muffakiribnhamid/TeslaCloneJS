5c228462de56601d95ad4126d03dbfbb18088d7d
muffakiribnhamid <muffakir63@gmail.com>
2023-07-12 14:16:18 +0530

    Addes JS Functionality

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function openMenu() {
+    document.querySelector(".backdrop").className = "backdrop active"
+    document.querySelector("aside").className = "active"
+}
+function closeMenu() {
+    document.querySelector(".backdrop").className = "backdrop"
+    document.querySelector("aside").className = ""
+}
+document.getElementById("menuBtn").onclick = e => {
+    e.preventDefault()
+    openMenu()
+}
+
+document.querySelector('aside button.close').onclick = e => {
+    closeMenu()
+}
+document.querySelector('.backdrop').onclick = e => {
+    closeMenu()
+}
